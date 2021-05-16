3bf6ca7443eb1a0b121833996e9bead578f576bb
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-05-16 14:51:39 +0530

    bug: amount on payment dashboard

--- src/App.js
+++ src/App.js
@@ -17,11 +17,16 @@ function App() {
       Come, play your part. Come, donate.</p>
     </div>
     <div className="donate-section">
+    <h2>DONATE HERE</h2>
+    <p>donate as you wish to help the people in need</p>
     <input type="number" placeholder="enter amount" onChange = {Event => setprice(Event.target.value) }/>
-    <StripeCheckout stripeKey="pk_test_51Io3U5SEwXz2aiklhqztYOutoVgCzVaEwTpNtWjmDlDWfxqorxS0555av2Dl4SIxt68mfBuQ3XDQ4YD2guHfLtXy00D4WJL37c" token="" name="DONATE" >
+    <StripeCheckout stripeKey="pk_test_51Io3U5SEwXz2aiklhqztYOutoVgCzVaEwTpNtWjmDlDWfxqorxS0555av2Dl4SIxt68mfBuQ3XDQ4YD2guHfLtXy00D4WJL37c" token="" name="DONATE usd" amount = {price*100}>
       <button className="btn-large">DONATE {price} $ </button>
     </StripeCheckout>
     </div>
+    <footer>
+    ©HelpInNeed , 2021 (this is an intenship project for <a href ="https://www.thesparksfoundationsingapore.org/">Sparks foundation</a> by <a href="https://www.linkedin.com/in/sumit-mehra-6579171b4/">Sumit Mehra</a>)
+    </footer>
     </>
   );
 }
